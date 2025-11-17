b613201fd679ac6215deacae8c61c5ffff94fb68
Jean Evenx Joseph <jeanevenx@gmail.com>
2025-11-17 01:35:14 -0300

    chore: atualizar dependências e remover diretórios vazios
    - Atualizado o package.json para incluir novas dependências:
    
      - @react-navigation/stack
      - drizzle-kit
      - drizzle-orm
      - expo-sqlite
      - lucide-react
      - lucide-react-native
      - radix-ui
    
    - Removidos arquivos .gitkeep não utilizados dos diretórios core, data, domain e presentation.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,4 @@
-import { Text, View } from "react-native";
+import { AppRegistry } from 'react-native';
+import App from '../src/App';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+AppRegistry.registerComponent("Inflow", () => App);
